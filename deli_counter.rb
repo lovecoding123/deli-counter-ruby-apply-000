e10f1318cfcd4822d9a4b586a7f331dfc2c92b12
Davonta Trench <trenchdavonta@gmail.com>
2019-11-04 16:32:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,4 @@
 def take_a_number(katz_deli, name)
   katz_deli << name
- puts "welcome, #{name} Your number #{katz_deli.length} in line." 
-end   
-
+ puts "welcome, #{name} Your number #{katz_deli.length} in line."
+end
